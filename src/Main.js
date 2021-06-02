6e8c42fe8bac7ed5b86e5f553d831a189c796180
wrightccc <mschincsk@gmail.com>
2021-06-02 18:22:43 +0800

    ui update

--- src/Main.js
+++ src/Main.js
@@ -1,34 +1,38 @@
 import React, { useEffect, useState } from "react";
+import './App.css';
 
 function Platform (props) {
-      // console.log(props.platforms)
-
-      // let platformId = props.platforms[0].platform_id
-      // console.log(platformId)
       if (!props.platforms) {
             return (
                   <div>
                         data not ava
                   </div>
-            )
-      }
-      
+      )}
       const platformInfo = props.platforms.map((p) => 
-            
-      <div className="py-2">
-            <div>Platform: {p.platform_id}</div>
-            {p.route_list.map((r) => 
+      <div className="py-3">
+            <div className="px-2 py-2 border border-white">
+                  <div className="py-1 text-white">Platform: {p.platform_id}</div>
                   <div className="px-2 row">
-                        <span className="col-3">{r.route_no} </span>
-                        <span className="col-6">{r.dest_en} </span>
-                        <span className="col-3">{r.time_en} </span>
+                              <span className="col-3 text-white">Route </span>
+                              <span className="col-3 text-white">Time </span>
+                              <span className="col-6 text-white">Destination </span>
                   </div>
-            )}
+                  {/* <hr className="bg-white"/> */}
+                  {p.route_list.map((r) => 
+                        <div className="px-2 row">
+                              <span className="col-3 text-success">{r.route_no} </span>
+                              <span className="col-3 text-warning">{r.time_en} </span>
+                              <span className="col-6 text-success">{r.dest_en} </span>
+                        </div>
+                  )}
+            </div>
       </div>
       );
       
       return(
-            <div>{platformInfo}</div>
+            <div>
+                  {platformInfo}
+            </div>
       )
 }
 
@@ -44,7 +48,6 @@ useEffect(() => {
                         .then((result) => {
                                     setIsLoaded(true);
                                     setItems(result);
-                                    // console.log(result)
                               });
             }, 10000);
       }, []);
@@ -56,11 +59,13 @@ if (error) {
 } else {
       return (
             <React.Fragment>
-                  <div className="container">
-                        <div className="pt-4">Refreshed at: {items.system_time}</div>
-                        <div>Station: Tuen Mun</div>
-                        <hr/>
-                        <Platform platforms={items.platform_list}/>
+                  <div className="bg-black">
+                        <div className="container">
+                              <div className="pt-4 text-white">Refreshed at: {items.system_time}</div>
+                              <div className="text-white">Station: Tuen Mun</div>
+                              {/* <hr className="bg-white"/> */}
+                              <Platform platforms={items.platform_list}/>
+                        </div>
                   </div>
             </React.Fragment>
             )
